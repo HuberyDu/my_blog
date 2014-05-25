849bcb5dd0cd2a0b1abeba4da543af1f42faf113
HuberyDu <duxiaolong92@gmail.com>
2014-05-25 12:05:47 +0800

    m

--- app/controllers/leave_messages_controller.rb
+++ app/controllers/leave_messages_controller.rb
@@ -2,7 +2,7 @@ class LeaveMessagesController < ApplicationController
   def create
     @leave_message = LeaveMessage.new(params[:leave_message])
     if @leave_message.save
-      UserMailer.leave_message(@leave_message).deliver
+      # UserMailer.leave_message(@leave_message).deliver
       redirect_to root_path, notice: t("leave_message_success", scope: "notice")
     else
       redirect_to root_path, alert: t("leave_message_failed", scope: "alert")
